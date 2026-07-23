4745cbc5fe5a4f03fab566a43240b5e985144470
alexg <alex.groat.95@gmail.com>
2026-07-23 20:45:22 +0930

    register app panel provider, add user role, add spatie roles and permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,18 +4,27 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasTenants;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasTenants
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+
+    use HasRoles;
+    use Notifiable;
 
     /**
      * Get the attributes that should be cast.
@@ -28,5 +37,25 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getTenants(Panel $panel): Collection
+    {
+        return $this->vets;
+    }
+
+    public function vets()
+    {
+        return $this->belongsToMany(Vet::class);
+    }
+
+    public function canAccessTenant(Model $tenant): bool
+    {
+        return $this->vets()->whereKey($tenant)->exists();
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
